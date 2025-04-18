69eeffbc30d9072eec44c0d7c4f7f6a809f20eeb
정병인 <byeongin.jeong@snetsystems.co.kr>
2025-04-18 14:16:58 +0900

    k8s 버전 업으로 인하여 설치 방법 변경경

--- k8s-server/k8s_common_install_ubuntu.sh
+++ k8s-server/k8s_common_install_ubuntu.sh
@@ -21,25 +21,45 @@ echo 0 > /proc/sys/vm/swappiness
 sed -e '/swap/ s/^#*/#/' -i /etc/fstab
 echo " "
 
-# 3. Download Docker Package (1.20.1)
-echo "################# Download k8s Package #################"
+# 3. Install Package
+echo "################# Install Package #################"
 echo " "
-wget https://packages.cloud.google.com/apt/pool/cri-tools_1.13.0-01_amd64_4ff4588f5589826775f4a3bebd95aec5b9fb591ba8fb89a62845ffa8efe8cf22.deb
-wget https://packages.cloud.google.com/apt/pool/kubeadm_1.20.1-00_amd64_7cd8d4021bb251862b755ed9c240091a532b89e6c796d58c3fdea7c9a72b878f.deb
-wget https://packages.cloud.google.com/apt/pool/kubectl_1.20.1-00_amd64_b927311062e6a4610d9ac3bc8560457ab23fbd697a3052c394a1d7cc9e46a17d.deb
-wget https://packages.cloud.google.com/apt/pool/kubelet_1.20.1-00_amd64_560a52294b8b339e0ca8ddbc480218e93ebb01daef0446887803815bcd0c41eb.deb
-wget https://packages.cloud.google.com/apt/pool/kubernetes-cni_0.8.7-00_amd64_ca2303ea0eecadf379c65bad855f9ad7c95c16502c0e7b3d50edcb53403c500f.deb
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl gpg
 echo " "
 
-# 4. Install Docker Package
-echo "################# Install k8s Package #################"
+# 4. Create GPG Key Directory and Add k8s GPG Key
+echo "################# Add GPG Key #################"
 echo " "
-apt-get install socat conntrack ebtables
-dpkg --install ./kubernetes-cni_0.8.7-00_amd64_ca2303ea0eecadf379c65bad855f9ad7c95c16502c0e7b3d50edcb53403c500f.deb
-dpkg --install ./kubelet_1.20.1-00_amd64_560a52294b8b339e0ca8ddbc480218e93ebb01daef0446887803815bcd0c41eb.deb
-dpkg --install ./cri-tools_1.13.0-01_amd64_4ff4588f5589826775f4a3bebd95aec5b9fb591ba8fb89a62845ffa8efe8cf22.deb
-dpkg --install ./kubectl_1.20.1-00_amd64_b927311062e6a4610d9ac3bc8560457ab23fbd697a3052c394a1d7cc9e46a17d.deb
-dpkg --install ./kubeadm_1.20.1-00_amd64_7cd8d4021bb251862b755ed9c240091a532b89e6c796d58c3fdea7c9a72b878f.deb
+mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo " "
+
+# 5. Add K8s Repo (1.29)
+echo "################# Add K8s Repo #################"
+echo " "
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo " "
+
+# 6. Install K8s (1.29)
+echo "################# Install K8s #################"
+echo " "
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+echo " "
+
+# 7. K8s Version Fixed(no Update)
+echo "################# Install K8s #################"
+echo " "
+apt-mark hold kubelet kubeadm kubectl
+echo " "
+
+# 8. containerd restart
+echo "################# ontainerd restart #################"
+echo " "
+rm /etc/containerd/config.toml
+systemctl restart containerd
 echo " "
 
 # Install Finish
